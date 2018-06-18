9b34e632ca4d3261eab966699696ab7f87dea2c2
KaijiS <31200760+KaijiS@users.noreply.github.com>
2018-06-18 13:28:00 +0900

    1.2

--- upload_form/views.py
+++ upload_form/views.py
@@ -10,8 +10,8 @@ DOWNLOADE_DIR = os.path.dirname(os.path.abspath(__file__)) + '/vowel_cut/wav_out
 import glob
 import subprocess
 
-from io import BytesIO  # 増えました
-import zipfile  # 増えました
+from io import BytesIO
+import zipfile
 
 from upload_form.vowel_cut import vowel_cut_main
 
@@ -30,7 +30,7 @@ def form(request):
     for file_name in file_list:
         subprocess.call(["rm", file_name])
 
-    # file = request.FILES['file']
+    # アップロードファイルの取得
     files = request.FILES.getlist("file[]")
     for file in files:
         path = os.path.join(UPLOADE_DIR, file.name)
@@ -42,6 +42,7 @@ def form(request):
         insert_data = FileNameModel(file_name = file.name)
         insert_data.save()
 
+    # 母音抽出及び、ファイルチェック
     common = vowel_cut_main.main(vowel)
 
     flag = 0
@@ -69,38 +70,27 @@ def form(request):
         'common':common
     }
 
-    # return redirect('upload_form:complete')
+
     return render(request, 'upload_form/complete.html',content)
 
 
-# def complete(request):
-#     vowel = request.GET.get('vowel')
-#     return render(request, 'upload_form/complete.html',{'vowel':vowel})
-
 
 def download(request):
-    # p_id = 11
-    # phoneme_obj = FileNameModel.objects.get(id=p_id)
     vowel = request.POST.get('vowel')
-
     common = request.POST.get('common')
 
 
     memory_file = BytesIO()
     phoneme_zip = zipfile.ZipFile(memory_file, 'w')
 
-    # DOWNLOADE_DIR を 切り出した波形が入ったディレクトリを指定する
     file_list = glob.glob(DOWNLOADE_DIR+'*')
 
     for file_part in file_list:
         file_name = file_part.replace(DOWNLOADE_DIR,"")
         phoneme_zip.writestr(file_name , open(file_part ,'rb').read())
-    # phoneme_zip.writestr(phoneme_obj.file_name, phoneme_obj.read())
-    # phoneme_zip.writestr(phoneme_obj.image2.name, phoneme_obj.image2.read())
-    # phoneme_zip.writestr(phoneme_obj.image3.name, phoneme_obj.image3.read())
+
 
     phoneme_zip.close()
     response = HttpResponse(memory_file.getvalue(), content_type="application/zip")
-    # 下のフォーマットカッコ内がzipファイルのフォルダ名
     response['Content-Disposition'] = 'attachment; filename={}_cut.zip'.format(common)
     return response
